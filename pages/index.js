a40b9f99fd5c82f8e02321a332a9ad5cc9bca839
hokyunrhee <hokyun.rhee@gmail.com>
2021-02-28 21:45:31 +0900

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
 import { useAuth } from "lib/auth";
+import { Button, Flex } from "@chakra-ui/react";
+import { createSite } from "@/lib/db";
 
 export default function Home() {
   const { user, signInWithGoogle, signOut } = useAuth();
   return (
-    <div>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
       {user ? (
-        <button onClick={signOut}>SignOut</button>
+        <Button as="a" href="/dashboard">
+          View Dashboard
+        </Button>
       ) : (
-        <button onClick={signInWithGoogle}>SignInWithGoogle</button>
+        <Button mt={4} size="sm" onClick={() => createSite({ test: 1 })}>
+          Sign In
+        </Button>
       )}
-    </div>
+    </Flex>
   );
 }

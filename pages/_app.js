9dc9a970043c04f1f357f71983118607d3b920bc
Ken Rhee <51858583+hokyunrhee@users.noreply.github.com>
2021-02-28 21:44:42 +0900

    chore: 절대 경로 임포트 설정 추가 (#4)

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { AuthProvider } from "lib/auth";
+import { AuthProvider } from "@/lib/auth";
+import { Global, css } from "@emotion/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS>
       <AuthProvider>
+        <Global
+          styles={css`
+            #__next {
+              display: flex;
+              flex-direction: column;
+              min-height: 100vh;
+            }
+          `}
+        />
         <Component {...pageProps} />
       </AuthProvider>
     </ChakraProvider>

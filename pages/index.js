2482b8b2fc84e4b18882e607d2c36cb9bcff693a
Ken Rhee <51858583+hokyunrhee@users.noreply.github.com>
2021-03-01 23:22:43 +0900

    feat: firebase admin 추가 (#5)
    * feat: firebase admin 추가
    
    * refactor: firebase private key 줄바꿈 추가

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,31 @@
-import { useAuth } from "@/lib/auth";
-import { Button } from "@chakra-ui/react";
+import { useAuth } from "lib/auth";
+import { Flex } from "@chakra-ui/react";
 
-export default function Home() {
+import { getAllFeedback, getSite } from "@/lib/db-admin";
+
+export default function Home({ allFeedback, site }) {
   const { user, signInWithGoogle, signOut } = useAuth();
   return (
-    <div>
-      {user ? (
-        <Button onClick={signOut}>SignOut</Button>
-      ) : (
-        <Button onClick={signInWithGoogle}>SignInWithGoogle</Button>
-      )}
-    </div>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    ></Flex>
   );
+}
+
+export async function getStaticProps(context) {
+  const SITE_ID = "1234";
+
+  const { feedback } = await getAllFeedback(SITE_ID);
+  const { site } = await getSite(SITE_ID);
+  return {
+    props: {
+      allFeedback: feedback,
+      site,
+    },
+    revalidate: 1,
+  };
 }

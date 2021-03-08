9b4ddb525b6f965c5150bbd0ac7f86215772596d
hokyunrhee <hokyun.rhee@gmail.com>
2021-03-08 15:34:57 +0900

    feat: sites api route 추가

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,28 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "@/lib/auth";
 import { Global, css } from "@emotion/react";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS>
       <AuthProvider>
-        <Global
-          styles={css`
-            #__next {
-              display: flex;
-              flex-direction: column;
-              min-height: 100vh;
-            }
-          `}
-        />
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Global
+            styles={css`
+              #__next {
+                display: flex;
+                flex-direction: column;
+                min-height: 100vh;
+              }
+            `}
+          />
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </AuthProvider>
     </ChakraProvider>
   );
